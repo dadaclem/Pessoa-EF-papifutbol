c491b1ff3d3c53967458ef8e3fc1bbd0f235b6c9
dadaclem <dadaclem@gmail.com>
2026-03-06 21:56:29 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client' // Assicurati che ci siano le graffe
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
